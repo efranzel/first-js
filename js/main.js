c426ef1d592751e263e9c8667cff0fce70c66c3d
Ethan Franzel <efranzel@comcast.net>
2018-01-25 20:02:51 -0500

    logging to the console

--- js/main.js
+++ js/main.js
@@ -13,3 +13,19 @@ function change(){
 var name = 'Lucille'; //store a string in a var
 var numberOfWidgets = 10; //number
 var isCodingCool = true; //boolean
+console.log(name); //log the value of a var to the console
+
+//store an array in a var
+var characters = [50, true, 'Lucille', 'Michael Bluth', 'Tobias', 10, 20];
+console.log(characters[3]);
+
+var deadSong = 'Dark Star';
+var yearOfBirth = 1966;
+var tired = true;
+console.log(deadSong);
+console.log(yearOfBirth);
+console.log(tired);
+
+var newcareer = ['librarian', 'ESL teacher', 'development officer', 'religious studies teacher']
+console.log(newcareer);
+console.log(newcareer[1]);
